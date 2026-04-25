e79241858b6eb8b15068c604cedb117ced3dd506
Dang Khanh Linh <email_cua_ban@example.com24126112@student.hcmute.edu.vn>
2026-04-25 19:17:02 +0700

    update config git action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/nhom02_vanphongpham",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
